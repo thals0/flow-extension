584332de4db6b886e6b1a6066b11a19a94ba7375
SoMin <thals0107@naver.com>
2023-05-27 15:54:00 +0900

    git init

---
+++ src/main/java/com/flow/extension/ExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.flow.extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtensionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtensionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flow/extension/ExtensionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.extension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
